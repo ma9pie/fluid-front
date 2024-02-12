2c3857fe2ff0957b86a58cd5d75a4ea0664caa1a
ma9pie <ma9pie.dev@gmail.com>
2024-02-12 17:23:02 +0900

    feat : 404 페이지 추가

---
+++ src/pages/404.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import tw, { styled } from 'twin.macro';
+
+import Head from '@/components/layout/Head';
+import Layout from '@/components/layout/Layout';
+
+const NotFound = () => {
+  return (
+    <Layout>
+      <Head title="Fluid - Not Found"></Head>
+      <Wrapper>
+        <Text>404</Text>
+        <Text>Not Found</Text>
+      </Wrapper>
+    </Layout>
+  );
+};
+
+export default NotFound;
+
+const Wrapper = styled.div`
+  ${tw`flex-1 flex flex-col justify-center items-center`};
+`;
+const Text = styled.p`
+  ${tw`text-5xl font-semibold text-brandColor`};
+  ${tw`md:(text-9xl)`};
+`;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,11 @@ const config: Config = {
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brandColor: 'var(--brandColor)',
+      },
+    },
   },
   plugins: [nextui()],
 };
