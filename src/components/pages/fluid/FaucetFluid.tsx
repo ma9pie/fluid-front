fb14d575c1482124af931a929ba1be14c23b0fd8
ma9pie <ma9pie.dev@gmail.com>
2024-02-15 20:30:56 +0900

    fix : FaucetFluid 변경

--- src/components/pages/fluid/FaucetFluid.tsx
+++ src/components/pages/fluid/FaucetFluid.tsx
@@ -1,10 +1,7 @@
-import { isAddress } from 'ethers';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import tw, { styled } from 'twin.macro';
 
 import TxRunButton from '@/components/common/buttons/TxRunButton';
-import Flex from '@/components/common/Flex';
-import AddressInput from '@/components/common/inputs/AddressInput';
 import Spacing from '@/components/common/Spacing';
 import Text from '@/components/common/Text';
 import { Card } from '@/components/nextui';
@@ -22,45 +19,12 @@ const FaucetFluid = () => {
     token: FLUID,
   });
 
-  const [address, setAddress] = useState('');
-  const [disableButton, setDisableButton] = useState(false);
-  const [buttonText, setButtonText] = useState('Faucet');
-
-  // Input 상태 관리
-  const handleChange = (value: string) => {
-    setAddress(value);
-  };
-
-  // 버튼 활성화 여부
-  useEffect(() => {
-    if (isLoading || !account || !isAddress(address)) {
-      setDisableButton(true);
-    } else {
-      setDisableButton(false);
-    }
-  }, [isLoading, account, address]);
-
-  // 버튼 텍스트 관리
-  useEffect(() => {
-    if (!address || isAddress(address)) {
-      setButtonText('Faucet');
-    } else {
-      setButtonText('Invalid address');
-    }
-  }, [address]);
-
-  // 입력값 초기화
-  const resetInputs = () => {
-    setAddress('');
-  };
-
   // Faucet fluid
   const handleClick = () => {
     runTx({
       txFn: () => faucetFluid(),
       onAfterTx: () => {
         refetchFluidBalance();
-        resetInputs();
       },
     });
   };
@@ -71,19 +35,12 @@ const FaucetFluid = () => {
         Faucet Fluid
       </Text>
 
-      <Spacing height={24}></Spacing>
-
-      <Flex col gap={8}>
-        <Text>Wallet Address</Text>
-        <AddressInput value={address} onChange={handleChange}></AddressInput>
-      </Flex>
-
       <Spacing height={64}></Spacing>
 
       <TxRunButton
-        buttonText={buttonText}
+        buttonText="Faucet"
         isLoading={isLoading}
-        disabled={disableButton}
+        disabled={!Boolean(account)}
         onClick={handleClick}
       ></TxRunButton>
     </Wrapper>
